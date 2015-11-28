c3958201960a506f11373765c972ae767401ff24
Fabio Collini <fabio.collini@gmail.com>
2015-11-28 17:30:39 +0100

    Fixed fab animation

--- app/src/main/java/com/example/android/testing/notes/util/SnackbarMessageManager.java
+++ app/src/main/java/com/example/android/testing/notes/util/SnackbarMessageManager.java
@@ -2,13 +2,15 @@ package com.example.android.testing.notes.util;
 
 import android.support.design.widget.Snackbar;
 
+import com.example.android.testing.notes.R;
+
 import it.cosenonjaviste.mv2m.Mv2mView;
 
 public class SnackbarMessageManager {
 
     public void showMessage(Mv2mView view, int message) {
         if (view != null) {
-            Snackbar.make(view.getActivity().findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG).show();
+            Snackbar.make(view.getActivity().findViewById(R.id.fab_add_notes), message, Snackbar.LENGTH_LONG).show();
         }
     }
 }
